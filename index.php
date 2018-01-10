03e24c6e586ad7c23987dce8da33743951c305d7
Kenneth Keyes <kkeyes1@cnm.edu>
2018-01-10 08:15:50 -0700

    Created table V1

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<titl>Snap-Tables</titl>
+	</head>
+	<body>
+		<table>
+			<tr>
+				<th>Language</th>
+				<th>Number of Users</th>
+				<th>Downside</th>
+			</tr>
+			<tr>
+				<td>Java</td>
+				<td>.NET</td>
+				<td>PHP</td>
+				<td>Ruby</td>
+			</tr>
+			<tr>
+				<td>125,718,403</td>
+				<td>64,371,473</td>
+				<td>75,383,142</td>
+				<td>1,403</td>
+			</tr>
+			<tr>
+				<td>Frameworks revolt! It's Java Spring!!</td>
+				<td>PHP forces to clean twice per week</td>
+				<td>Misunderstood by Java and .NET Developers</td>
+				<td>Only for Hipster Driven Development (HDD)</td>
+			</tr>
+		</table>
+	</body>
+</html>
